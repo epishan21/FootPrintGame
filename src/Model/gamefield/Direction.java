086a173bc296e3120d38090af4aa68cd02e0a03f
Dmitry Epishkin <dimka.epishkin@gmail.com>
2022-04-01 19:24:24 +0300

    Реализованы функции класса  Direction

--- src/Model/gamefield/Direction.java
+++ src/Model/gamefield/Direction.java
@@ -8,53 +8,64 @@ public class Direction {
     private final double _hours;
 
     private Direction(double hours) {
+
+        // Приводим заданные часы к допустимому диапазону
+        hours = hours % 12;
+        if (hours < 0) { hours += 12; }
+
         _hours = hours;
     }
 
     // ----------------------- Порождение --------------------------
-    public static Direction north() {
-        return null;
-    }
+    public static Direction north() { return new Direction(0); }
 
     public static Direction northEast() {
-        return null;
+        return new Direction(1.5);
     }
 
     public static Direction southEast() {
-        return null;
+        return new Direction(4.5);
     }
 
-    public static Direction south() {
-        return null;
-    }
+    public static Direction south() { return new Direction(6); }
 
     public static Direction southWest() {
-        return null;
+        return new Direction(7.5);
     }
 
     public static Direction northWest() {
-        return null;
+        return new Direction(10.5);
     }
 
     public static Direction[] directions = { north(), northEast(), southEast(), south(), southWest(), northWest() };
 
     // ------------------ Новые направления ---------------------
     public Direction opposite() {
-        return null;
+        return new Direction(this._hours + 6);
     }
 
+
     // ------------------ Сравнение направлений ---------------------
-
-
     public int hashCode() {
-       return 0;
+        double hash = 5;
+        hash = 53 * hash + this._hours;
+        int b = (int) hash;
+        return b;
     }
 
     @Override
     public boolean equals(Object other) {
-        return false;
-    }
+        if (other == null) {
+            return false;
+        }
 
-    // --------------------------------------------------
+        if (!(other instanceof Direction)) {
+            // если объект не совместим c Direction, возвращаем false
+            return false;
+        }
+
+        Direction direct = (Direction) other;
+        return _hours == direct._hours;
+    }
 
 }
