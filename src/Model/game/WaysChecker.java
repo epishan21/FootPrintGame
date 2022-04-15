a57dfa85c0954c9a5ccd1ba484364ca88f800954
epishan21 <dimka.epishkin@gmail.com>
2022-04-15 21:42:32 +0300

    Улучшен функционал функции CheckWay
    Теперь CheckWay может проверять путь от робота до заданной позиции.

--- src/Model/game/WaysChecker.java
+++ src/Model/game/WaysChecker.java
@@ -1,13 +1,16 @@
 package Model.game;
-import Model.gamefield.*;
 
-import java.util.*;
+import Model.gamefield.CellPosition;
+import Model.gamefield.Direction;
+import Model.gamefield.cells.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 // Проверка на наличие пути от робота до целевого шестиугольника
 public class WaysChecker {
 
-    public boolean CheckWay(CellPosition posRobot, HashMap<CellPosition, AbstractCell> _cells) {
+    public boolean CheckWay(CellPosition posRobot, CellPosition posCell, HashMap<CellPosition, AbstractCell> _cells) {
 
         AbstractCell robotCell = _cells.get(posRobot); // Ячейка с роботом
 
@@ -18,16 +21,21 @@ public class WaysChecker {
         // Пока есть что проверять
         while (!toCheckCells.isEmpty()) {
             AbstractCell currentCell = toCheckCells.get(0);
+            // Если Робот стоит на искомой ячейке
+            if (currentCell != null && currentCell.position().row() == posCell.row() && currentCell.position().column() == posCell.column()) {
+                return true;
+            }
 
             // Для каждого направления ячейки
             for (Direction direction : Direction.directions) {
                 AbstractCell nextCell = currentCell.neighbor(direction);
 
-                // Если сосед является целевым  шестиугольником
-                if(nextCell != null && nextCell.getClass().equals(TargetHexagon.class))
+                // Если сосед является искомой ячейкой
+                if (nextCell != null && nextCell.position().row() == posCell.row() && nextCell.position().column() == posCell.column()) {
                     return true;
+                }
                 // Иначе если соседняя ячейка имеется и она не является следом цвета игрока и она еще не проверена
-                else if (nextCell != null &&  ((Cell)nextCell).getFootprint() != robotCell.getRobot().getColor() && !checkedCells.contains(nextCell)) {
+                else if (nextCell instanceof UnitCell && (nextCell.getClass().equals(PassableCell.class) || nextCell.getClass().equals(TargetHexagon.class) || ((FootprintCell) nextCell).getFootprint() != ((UnitCell) robotCell).getRobot().getColor()) && !checkedCells.contains(nextCell)) {
                     // Добавить в список ячеек которые надо проверить
                     toCheckCells.add(nextCell);
                 }
@@ -38,6 +46,7 @@ public class WaysChecker {
 
             // Удалить currentCell из списка ячеек которые надо проверить
             toCheckCells.remove(0);
+
         }
         return false;
     }
